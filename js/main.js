f53fe0c7b4e1a02df43c3567087aa2de9b51afaf
Jamil Samarani <jamilsamarani@hotmail.com>
2020-07-23 09:18:46 +1000

    winning symbol recognised. win and draw function and reset button added.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
-// const winCombo = [
-//
-// ]
 $(document).ready(function(){
 
   console.log('Tic Tac Toe');
 
+const playerOne = 'X';
+const playerTwo = 'O';
+// const winCombo = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+// ];
 
-
-  // Do something whenever the mouse enters the board, and
-  // something else when the mouse leaves
-  $('#board').hover(
-    function(){
-      console.log('mouse entered area');
-    },
-    function(){
-      console.log('mouse left area');
-    }
-  );
+let currentTurn = "O"; //sets player initial value
+let turnCounter = 0;//
 
   // Do something whenever the user clicks on the game board
   // $('#board').click( fn ); <-- shortcut convenience method
@@ -25,9 +25,65 @@ $(document).ready(function(){
     const activeCell = $(this).attr('id');
     // This code will run whenever the #board element is clicked
     // The function will re-run EACH click
-    $(ev.target).html('X');
-    console.log(activeCell);
-  });
+
+
+      const contents = $(this).html(); //check contents of cell
+
+      //test if cell contains X or O in it.
+      if (contents === "X" || contents === "O"){
+      //leave the function early if the cell is occupied, which prevents the turn taking code and turn switching code from being executed.
+      return;
+
+      }// contents checker
+
+    //turn taking code
+    $(this).html(currentTurn); //this means we can change the turn, using currentTurn as a starting variable
+
+
+
+    //Check if turn caused the game to be won.
+
+  turnCounter += 1; //add 1 to turn counter after each turn
+  console.log(turnCounter);
+
+  if (($("#0").html() !== "" && $("#0").html() === $("#1").html() && $("#1").html() === $("#2").html()) ||
+     ($("#3").html() !== "" && $("#3").html() === $("#4").html() && $("#4").html() === $("#5").html()) ||
+     ($("#6").html() !== "" && $("#6").html() === $("#7").html() && $("#7").html() === $("#8").html()) ||
+     ($("#0").html() !== "" && $("#0").html() === $("#3").html() && $("#3").html() === $("#6").html()) ||
+     ($("#1").html() !== "" && $("#1").html() === $("#4").html() && $("#4").html() === $("#7").html()) ||
+     ($("#2").html() !== "" && $("#2").html() === $("#5").html() && $("#5").html() === $("#8").html()) ||
+     ($("#0").html() !== "" && $("#0").html() === $("#4").html() && $("#4").html() === $("#8").html()) ||
+     ($("#2").html() !== "" && $("#2").html() === $("#4").html() && $("#4").html() === $("#6").html()))
+
+  {
+    // console.log(currentTurn);
+    console.log("End of Game!");
+
+    // document.getElementById("endGame").style.zIndex = "-1";
+
+  } else if(turnCounter === 9){
+    console.log("DRAW");
+  }
+
+
+  // if (currentTurn === "X") {
+  //   console.log("playerOneWinMsg");
+  // } else if (currentTurn === "O"){
+  //   console.log("playerTwoWinMsg");
+  // }
+
+
+  //turn switching code
+  // if currentTurn === O (turn just played) then the next "currentTurn = X"
+  //else currentTurn === X, then change to currentTurn to "O"
+  if (currentTurn === "O") {
+    currentTurn = "X";
+  } else {
+    currentTurn = "O";
+  }//else
+//
+    // console.log(activeCell);
+  });//end click function
 
 
 
@@ -36,6 +92,8 @@ $(document).ready(function(){
   $('#restartButton').on( 'click', function(){
     // This code will run whenever the #board element is clicked
     // The function will re-run EACH click
+    $(".cell").html("");
+    turnCounter = 0;
     console.log('Restart button clicked!');
   });
 
@@ -62,3 +120,17 @@ $(document).ready(function(){
 //   $(this).html('i was clicked');
 //
 // });
+
+
+
+
+// Do something whenever the mouse enters the board, and
+// something else when the mouse leaves
+// $('#board').hover(
+  //   function(){
+    //     console.log('mouse entered area');
+    //   },
+    //   function(){
+      //     console.log('mouse left area');
+      //   }
+      // );
